c18f784d5e326a288ff0c0fd8eb4208f82ba2fb0
Melisa Akbulut <melisakbulutk@gmail.com>
2024-08-22 20:12:23 +0300

    first commit

---
+++ C++/basic_data_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int a;
+	long l;
+	char ch;
+	float f;
+	double d;
+	scanf("%d %ld %c %f %lf", &a, &l, &ch, &f, &d);
+	printf("%d \n %ld \n %c \n %f \n %lf", a, l, ch, f, d);
+	return 0;
+}
---
+++ C++/input_and_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a=1;
+	int b=2;
+	int c=7;
+	cin >> a >> b >> c;
+	if (1<=a<=1000 & 1<=b<=1000 & 1<=c<=1000 ) 
+	{
+		cout << a+b+c;
+	}
+	return 0;
+}
