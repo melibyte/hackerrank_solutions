a291f07710eff9b4be899e4cfeaea848d3a780c8
Melisa Akbulut <melisakbulutk@gmail.com>
2025-01-23 14:41:42 +0300

    Added Introduction Projects

---
+++ Python/print_function.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    k = range(1, n+1)
+    for i in k:
+        print (i, end="")
---
+++ Python/ıf_else.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    if n%2 == 1:
+        print("Weird")
+    elif n%2 == 0 and n in range(2,6):
+        print("Not Weird")
+    elif n%2 == 0 and n in range(6,21):
+        print("Weird")
+    elif n%2 == 0 and n in range(21,101):
+        print("Not Weird")
